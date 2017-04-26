44f5d6d5143ae85cae7b42bd45a6456c24e65939
joselineem <joselineem@gmail.com>
2017-04-26 15:01:58 -0600

    randomuser-Parte3

--- randomuser/index.js
+++ randomuser/index.js
@@ -8,8 +8,22 @@ function datosRandom(){
  	 url: 'https://randomuser.me/api/',
  	 dataType: 'json',
  	 success: function(data) {
- 	   $("#txtNombre").html(data.results[0].name.first+" "+data.results[0].name.last)
- 	 $("#imgFoto").attr("src",data.results[0].picture.large)
+ 	   $("#txtNombre").html(data.results[0].name.first+" "
+ 	   						+data.results[0].name.last)
+ 	   $("#imgFoto").attr("src",data.results[0].picture.large)
+ 	   $("#txtLocation").html(data.results[0].location.street+", "
+ 	   							+data.results[0].location.city+", "
+ 	   							+data.results[0].location.state)
+ 	   $("#txtEmail").html(data.results[0].email)
+ 	   $("#txtLogin").html("Username: "+data.results[0].login.username+
+ 	   						"<br>Password: "+data.results[0].login.password)
+ 	   $("#txtDob").html(data.results[0].dob)
+ 	   $("#txtRegistered").html(data.results[0].registered)
+ 	   $("#txtPhone").html(data.results[0].phone)
+ 	   $("#txtCell").html(data.results[0].cell)
+ 	   $("#txtId").html(data.results[0].id.name+" "+ data.results[0].id.value)
+ 	   $("#txtNat").html(data.results[0].nat)
+
  	 },
  	 error(a,b,c){
   		alert("Sin internet o servidor");
